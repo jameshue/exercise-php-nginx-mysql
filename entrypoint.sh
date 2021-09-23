03745daee38d605a73f04382d7380dd0828ef82e
James Xu <jameshue@hotmail.com>
2021-09-23 12:10:29 +0800

    add composer-install cmd in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run supervisord
-exec /usr/bin/supervisord -n -c /etc/supervisord.conf
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf;chown -fR www-data. /app ;composer install
