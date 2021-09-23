04e132293cd9c1f224852fbfffaceeb456e681bb
James Xu <jameshue@hotmail.com>
2021-09-23 12:55:21 +0800

    Update db_backup.sh

--- backup/db_backup.sh
+++ backup/db_backup.sh
@@ -11,6 +11,6 @@ now=mysql-$(date +"%Y-%m-%d-%H")-all_db.sql.gz
 # Backup database.
 source ../.env.db
 docker exec -i $(docker ps -qf name=webapp-db) bash -c 'mysqldump --default-character-set=utf8mb4 -u root -p${MYSQL_PASSWORD} --all-databases 2>/dev/null' |gzip -c > ./db/${now}
-#docker exec -i $(docker ps -qf name=webapp-db) bash -c 'mysqldump --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} --all-databases 2>/dev/null' |gzip -c > ./db/${now}
+#docker exec -i $(docker ps -qf name=webapp-db) bash -c 'mysqldump --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} 2>/dev/null' |gzip -c > ./db/${now}
 befor=mysql-$(date +"%Y-%m-%d-%H" --date="-11 day")-all_db.sql.gz
 rm -fR ./db/$befor
