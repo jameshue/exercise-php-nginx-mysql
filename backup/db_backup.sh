77b32610eab71e63cdd8853ba37299510414820c
James Xu <jameshue@hotmail.com>
2021-09-23 12:58:44 +0800

    Update db_backup.sh

--- backup/db_backup.sh
+++ backup/db_backup.sh
@@ -7,10 +7,11 @@
 
 # Get current date.
 now=mysql-$(date +"%Y-%m-%d-%H")-all_db.sql.gz
+befor=mysql-$(date +"%Y-%m-%d-%H" --date="-11 day")-all_db.sql.gz
 
 # Backup database.
 source ../.env.db
 docker exec -i $(docker ps -qf name=webapp-db) bash -c 'mysqldump --default-character-set=utf8mb4 -u root -p${MYSQL_PASSWORD} --all-databases 2>/dev/null' |gzip -c > ./db/${now}
 #docker exec -i $(docker ps -qf name=webapp-db) bash -c 'mysqldump --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} 2>/dev/null' |gzip -c > ./db/${now}
-befor=mysql-$(date +"%Y-%m-%d-%H" --date="-11 day")-all_db.sql.gz
+# Delete the backup file 10 days ago
 rm -fR ./db/$befor
