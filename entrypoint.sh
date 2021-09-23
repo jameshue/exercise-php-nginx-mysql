d8e356452e7c7e73e46f105586d13a69be9d6387
James Xu <jameshue@hotmail.com>
2021-09-23 12:19:52 +0800

    fix sqldb init process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run supervisord
-exec /usr/bin/supervisord -n -c /etc/supervisord.conf;chown -fR www-data. /app ;composer install
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
